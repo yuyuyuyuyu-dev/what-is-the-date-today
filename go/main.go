0fe5b20271545992967debd84c1250093bc5b7e4
yu-ko-ba <lily_lime_mint@outlook.com>
2021-12-31 13:31:17 +0900

    曜日を表示するようにした

--- go/main.go
+++ go/main.go
@@ -21,14 +21,24 @@ func registerCallbacks() {
 
 func getDate(js.Value, []js.Value) interface{} {
     // 現在日時を取得する
-    now := time.Now().Format("2006/01/02")
-    arr := strings.Split(now, "/")
+    now := time.Now()
+    arr := strings.Split(now.Format("2006/01/02"), "/")
 
     // 和暦を計算する
     japaneseYear, _ := strconv.Atoi(arr[0])
     japaneseYear -= 2018
-    // 年のうしろに和暦を追加する
-    arr[0] = arr[0] + "（令和" + strconv.Itoa(japaneseYear) + "年）"
 
-    return strings.Join(arr, "/")
+    // 年月日の文字を追加する
+    arr[0] = arr[0] + "年"
+    arr[1] = arr[1] + "月"
+    arr[2] = arr[2] + "日"
+
+    // 年のうしろに和暦を追加する
+    arr[0] = arr[0] + "(令和" + strconv.Itoa(japaneseYear) + "年) "
+
+    // 曜日を追加する
+    dayOfWeek := []string{"日", "月", "火", "水", "木", "金", "土"}
+    arr = append(arr, " " + dayOfWeek[now.Weekday()] + "曜日")
+
+    return strings.Join(arr, "")
 }
