a9438fa126fe0ce2bad1f7abdbc1c7079bb1f6d6
yu-ko-ba <lily_lime_mint@outlook.com>
2021-12-31 13:12:39 +0900

    日付を表示するようにした

--- go/main.go
+++ go/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-    "fmt"
+	"strconv"
+	"strings"
+	"syscall/js"
+	"time"
 )
 
 func main() {
-    fmt.Println("Hello, world!")
+    c := make(chan struct{}, 0)
+    registerCallbacks()
+    <-c
+}
+
+
+func registerCallbacks() {
+    js.Global().Set("getDate", js.FuncOf(getDate))
+}
+
+
+func getDate(js.Value, []js.Value) interface{} {
+    // 現在日時を取得する
+    now := time.Now().Format("2006/01/02")
+    arr := strings.Split(now, "/")
+
+    // 和暦を計算する
+    japaneseYear, _ := strconv.Atoi(arr[0])
+    japaneseYear -= 2018
+    // 年のうしろに和暦を追加する
+    arr[0] = arr[0] + "（令和" + strconv.Itoa(japaneseYear) + "年）"
+
+    return strings.Join(arr, "/")
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+function runButtonOnClicked() {
+    console.log(getDate());
+}
+
+
+function main() {
+    const date = document.getElementById("date");
+    date.textContent = getDate();
+}
+
+
+mainLoaded = true;
+if (mainLoaded == wasmLoaded) {
+    main();
+}
